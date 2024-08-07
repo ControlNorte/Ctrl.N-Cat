7f7e2613ce173ba419b6e1241f44bba83f368226
ControlNorte <ctrl.norte@gmail.com>
2024-08-07 18:10:11 -0300

    ajuste default db video

--- ControlNorte/settings.py
+++ ControlNorte/settings.py
@@ -90,9 +90,13 @@ WSGI_APPLICATION = 'ControlNorte.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': 'postgres',
+            'PASSWORD': 'rJAVyBfPxCTZWlHqnAOTZpmwABaKyaWg',
+            'HOST': 'postgres.railway.internal',
+            'PORT': '5432',
+        }
 }
 
 
