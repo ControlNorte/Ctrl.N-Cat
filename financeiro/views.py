6184087bfa037c4b4b06ba5ab732d061212a5eea
ControlNorte <ctrl.norte@gmail.com>
2024-08-10 15:04:23 -0300

    ajuste das transferencias e exclusões na conciliação

--- financeiro/views.py
+++ financeiro/views.py
@@ -174,7 +174,7 @@ def save_data(request):
         valor = float(valor.replace('.', '').replace(',', '.'))
         try:
             movimentacoes_to_create.append(MovimentacoesCliente(
-                        cliente=cliente,
+                        cliente=cadastro_de_cliente.objects.get(id=cliente),
                         banco=BancosCliente.objects.get(id=banco),
                         data=data,
                         descricao=descricao,
@@ -186,7 +186,7 @@ def save_data(request):
             ))
         except:
             movimentacoes_to_create.append(MovimentacoesCliente(
-                            cliente=cliente,
+                            cliente=cadastro_de_cliente.objects.get(id=cliente),
                             banco=BancosCliente.objects.get(id=banco),
                             data=data,
                             descricao=descricao,
