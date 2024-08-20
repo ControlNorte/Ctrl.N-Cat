d5f5515d07bf60e932215bab3e66b6c7446c690a
ControlNorte <ctrl.norte@gmail.com>
2024-08-19 22:06:57 -0300

    ajuste na conciliação

--- financeiro/teste.py
+++ financeiro/teste.py
@@ -143,7 +143,7 @@ def importar_arquivo_excel(arquivo_upload, cliente, banco, request):
             saldo_inicial = Saldo.objects.filter(cliente=cliente, banco=banco, data=current_date - timedelta(days=1)).first()
             saldo_inicial = saldo_inicial.saldofinal if saldo_inicial else 0  # Obtém o saldo final do dia anterior
 
-            saldo_movimentacoes = MovimentacoesCliente.objects.filter(cliente=cliente, banco=banco, data=current_date).aggregate(total_movimentacoes=pd.Sum('valor'))['total_movimentacoes'] or 0
+            saldo_movimentacoes = MovimentacoesCliente.objects.filter(cliente=cliente, banco=banco, data=current_date).aggregate(total_movimentacoes=pd.sum('valor'))['total_movimentacoes'] or 0
             saldo_final = saldo_inicial + saldo_movimentacoes
 
             saldo_atualizacoes.append(Saldo(
