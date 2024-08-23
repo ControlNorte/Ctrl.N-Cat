4adbc69f1db48687e7694d334c6419856e480629
ControlNorte <ctrl.norte@gmail.com>
2024-08-23 13:17:01 -0300

    ajuste na importacao

--- financeiro/alteracoesdb.py
+++ financeiro/alteracoesdb.py
@@ -83,6 +83,7 @@ def alteracaosaldo(banco, cliente, data):
         saldo_final = saldo_inicial + saldo_movimentacoes
 
         print(f'SI: {saldo_inicial}, SF: {saldo_final}, data: {datainicial}')
+
         print(type(datainicial))
 
         with connection.cursor() as cursor:
