5b28289ea082e3deac5011687d0e38e319d8811f
ControlNorte <ctrl.norte@gmail.com>
2024-08-28 20:39:11 -0300

    ajuste na importacao

--- hpinicial/middleware.py
+++ hpinicial/middleware.py
@@ -2,11 +2,11 @@ from django.utils.deprecation import MiddlewareMixin
 from .models import Tenant
 
 
-class TenantMiddleware(MiddlewareMixin):
-
-    def process_request(self, request):
-        subdomain = request.get_host().split('.')[0]
-        try:
-            request.tenant = Tenant.objects.get(subdomain=None)
-        except Tenant.DoesNotExist:
-            request.tenant = None
+# class TenantMiddleware(MiddlewareMixin):
+#
+#     def process_request(self, request):
+#         subdomain = request.get_host().split('.')[0]
+#         try:
+#             request.tenant = Tenant.objects.get(subdomain=None)
+#         except Tenant.DoesNotExist:
+#             request.tenant = None
