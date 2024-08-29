dc1433a0f4a9417e758d1b3a0ebdc109b4aef2e9
ControlNorte <ctrl.norte@gmail.com>
2024-08-28 22:46:49 -0300

    ajuste na importacao

--- ControlNorte/settings.py
+++ ControlNorte/settings.py
@@ -111,7 +111,7 @@ if DATABASE_URL:
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-# AUTH_USER_MODEL = "hpinicial.Usuario"
+AUTH_USER_MODEL = "hpinicial.Usuario"
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
