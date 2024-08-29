43a5fab7ce98c51320d1cdf77fb1fbfc068c3df6
ControlNorte <ctrl.norte@gmail.com>
2024-08-29 09:29:44 -0300

    ajuste na importacao

--- hpinicial/middleware.py
+++ hpinicial/middleware.py
@@ -6,7 +6,10 @@ class TenantMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         subdomain = request.get_host().split('.')[0]
+        print(subdomain)
         try:
             request.tenant = Tenant.objects.get(subdomain=subdomain)
+            print(request.tenant)
         except Tenant.DoesNotExist:
             request.tenant = None
+            print(None)
