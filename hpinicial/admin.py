b4d338dcae3b4bf5df8d45b982e3df1d5a671926
ControlNorte <ctrl.norte@gmail.com>
2024-08-29 09:16:54 -0300

    ajuste na importacao

--- hpinicial/admin.py
+++ hpinicial/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 campos = list(UserAdmin.fieldsets)
-campos.append(("Informações pessoais", {"fields":("cpf",)}))
+campos.append(("Informações pessoais", {"fields":("cpf", "tenant_id")}))
 
 UserAdmin.fieldsets = tuple(campos)
 
