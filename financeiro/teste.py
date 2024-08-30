a7edf4b779865d53b96082af579bbe0adb26271a
ControlNorte <ctrl.norte@gmail.com>
2024-08-30 16:07:13 -0300

    ajuste na importacao

--- financeiro/teste.py
+++ financeiro/teste.py
@@ -164,7 +164,8 @@ def importar_arquivo_excel(arquivo_upload, cliente, banco, request):
             saldo_final = saldo_inicial + saldo_movimentacoes
 
             tenant = int(request.tenant.id)
-            print(cliente)
+            print(type(cliente))
+            cliente = int(cliente)
             banco = banco.id
 
             with connection.cursor() as cursor:
