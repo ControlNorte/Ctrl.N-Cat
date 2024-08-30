51150ce75d0b8fc2ae612336ed5b7b9a6523efa2
ControlNorte <ctrl.norte@gmail.com>
2024-08-30 13:17:42 -0300

    ajuste na importacao

--- financeiro/teste.py
+++ financeiro/teste.py
@@ -94,12 +94,12 @@ def importar_arquivo_excel(arquivo_upload, cliente, banco, request):
     # Processamento das transações
     for dado in dados_dict:
         descricao = dado['Descrição'].upper()
-        print(f'cliente: {cliente} \n banco: {banco} \n data: {dado['Data']} \n descrição: {descricao} \n valor: {dado['Valor']}')
 
         # Verifica se já existe uma movimentação com a mesma data, descrição e valor
         if MovimentacoesCliente.objects.for_tenant(request.tenant).filter(cliente=cliente, banco=banco, data=dado['Data'], descricao=descricao,
                                                valor=dado['Valor']).exists():
-
+            print(MovimentacoesCliente.objects.for_tenant(request.tenant).filter(cliente=cliente, banco=banco, data=dado['Data'], descricao=descricao,
+                                               valor=dado['Valor']))
             continue  # Pula para o próximo dado se já existir uma movimentação igual
 
         matched = False  # Indicador de correspondência
