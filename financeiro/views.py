e62c8afa812fdaf0215e8a41a5ab9898665cda2b
ControlNorte <ctrl.norte@gmail.com>
2024-09-12 22:11:09 -0300

    ajuste na importacao

--- financeiro/views.py
+++ financeiro/views.py
@@ -487,9 +487,6 @@ def contas(request):
                             centro_custo=centro_custo, categoria=categoria,
                             sub_categoria=sub_categoria, valor=valor)
 
-        return pesquisas
-    for pesquisa in pesquisas:
-        print(pesquisa)
     dadoscliente = cadastro_de_cliente.objects.for_tenant(request.tenant).get(pk=pk)
     movimentacoes = MovimentacoesCliente.objects.for_tenant(request.tenant).filter(cliente=dadoscliente).order_by('id')
     paginator = Paginator(movimentacoes, 100)
