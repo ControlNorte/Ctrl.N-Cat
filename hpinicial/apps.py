bd5bac99464c5955bedf199065928568a0a079be
ControlNorte <ctrl.norte@gmail.com>
2025-01-04 10:52:55 -0300

    0001-ADD SUPERUSER

--- hpinicial/apps.py
+++ hpinicial/apps.py
@@ -14,4 +14,4 @@ class HpinicialConfig(AppConfig):
 
         usuarios = Usuario.objects.filter(email=email)
         if not usuarios:
-            Usuario.objects.creat_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
