5ff79a73faae6011d5dfec2b9cdf582f07ac2b3b
ControlNorte <ctrl.norte@gmail.com>
2025-01-04 10:30:06 -0300

    0001-ADD SUPERUSER

--- hpinicial/apps.py
+++ hpinicial/apps.py
@@ -1,6 +1,18 @@
+from Demos.win32ts_logoff_disconnected import username
 from django.apps import AppConfig
 
 
 class HpinicialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hpinicial'
+
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            usuarios.objects.creat_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
