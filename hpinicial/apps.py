8e10dea75a34c5f499fc6e33042198c880426bba
ControlNorte <ctrl.norte@gmail.com>
2025-01-06 21:04:04 -0300

    0000

--- hpinicial/apps.py
+++ hpinicial/apps.py
@@ -5,11 +5,11 @@ class HpinicialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hpinicial'
 
-    # def ready(self):
-    #     from .models import Usuario
-    #     import os
-    #
-    #     email = os.getenv("EMAIL_ADMIN")
-    #     senha = os.getenv("SENHA_ADMIN")
-    #
-    #     Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
