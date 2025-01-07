0a19ffd41dfe542d77f44d3311f19f683965a53b
ControlNorte <ctrl.norte@gmail.com>
2025-01-06 21:35:22 -0300

    0000

--- hpinicial/models.py
+++ hpinicial/models.py
@@ -7,6 +7,7 @@ class Tenant(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     subdomain = models.CharField(max_length=255, unique=True)
     cnpj = models.CharField(max_length=255, unique=True)
+    CNPJ = models.CharField(max_length=255, unique=True, null=True, blank=True)
     ativo = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
