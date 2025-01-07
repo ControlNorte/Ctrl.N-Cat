228cd02776a5bb2dd268fd7680684604e276fa89
ControlNorte <ctrl.norte@gmail.com>
2025-01-06 21:28:27 -0300

    0000

--- hpinicial/models.py
+++ hpinicial/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Tenant(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     subdomain = models.CharField(max_length=255, unique=True)
-    cnpj = models.IntegerField(unique=True)
+    cnpj = models.CharField(max_length=255, unique=True)
     ativo = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
