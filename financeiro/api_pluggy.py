5692caba5c76fbc0a531f9a5fcf84639683b1bdd
ControlNorte <ctrl.norte@gmail.com>
2025-01-18 11:35:21 -0300

    0002 - Conexão Pluggy

--- financeiro/api_pluggy.py
+++ financeiro/api_pluggy.py
@@ -18,13 +18,18 @@ def concexao_api():
     api_key = response.text
     print(api_key)
 
+    payload = {
+        "clientId": "226a2d88-095c-4469-9943-1a3e6e3ae477",
+        "clientSecret": "58b103c9-2272-4f7d-a1ef-80dd015704dc"
+    }
+
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
         "X-API-KEY": api_key
     }
 
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
 
     access_token = response.text
     print(access_token)
