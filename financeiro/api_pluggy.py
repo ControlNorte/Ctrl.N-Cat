e99b1ede9889c97e3169d1ee49f2b3a05c26a1b5
ControlNorte <ctrl.norte@gmail.com>
2025-01-18 10:33:39 -0300

    0002 - Conexão Pluggy

---
+++ financeiro/api_pluggy.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.pluggy.ai/auth"
+
+payload = {
+    "clientId": "226a2d88-095c-4469-9943-1a3e6e3ae477",
+    "clientSecret": "58b103c9-2272-4f7d-a1ef-80dd015704dc"
+}
+headers = {
+    "accept": "application/json",
+    "content-type": "application/json"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+print(response.text)
