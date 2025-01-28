d9a38621562d913d69de82a53bc007cd00cc03aa
ControlNorte <ctrl.norte@gmail.com>
2025-01-28 10:09:38 -0300

    0002 - Conexão Pluggy

--- ControlNorte/settings.py
+++ ControlNorte/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'railway',
             'USER': 'postgres',
-            'PASSWORD': 'oMRdpzVBqmcECNemqZsMksvkXBXCrfse',
+            'PASSWORD': 'mdcOQHlvdMKsOBUxIBQJgeuDcugbAhjh',
             'HOST': 'postgres.railway.internal',
             'PORT': '5432',
         }
