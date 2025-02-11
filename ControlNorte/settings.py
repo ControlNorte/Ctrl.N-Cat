1d376d50cf8829f647cbc5ec76ab79bf53b768e5
ControlNorte <ctrl.norte@gmail.com>
2025-02-11 19:20:14 -0300

    0002 - Conexão Pluggy

--- ControlNorte/settings.py
+++ ControlNorte/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'financeiro.middleware.ClienteMiddleware',
     'hpinicial.middleware.TenantMiddleware',
 ]
 
