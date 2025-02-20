65a92feb85c2503d01826745f6b8c327cd9e6d8d
ControlNorte <ctrl.norte@gmail.com>
2025-02-19 22:35:21 -0300

    0002 - Conexão Pluggy

--- ControlNorte/asgi.py
+++ ControlNorte/asgi.py
@@ -12,7 +12,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from financeiro.routing import websocket_urlpatterns  # Definiremos esse arquivo depois
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seu_projeto.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ControlNorte.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
