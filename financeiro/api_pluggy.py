24470363ab72346a741b61fb2dc5ed34028ccf1f
ControlNorte <ctrl.norte@gmail.com>
2025-03-24 20:13:17 -0300

    0002 - Conexão Pluggy

--- financeiro/api_pluggy.py
+++ financeiro/api_pluggy.py
@@ -371,18 +371,10 @@ def process_webhook(webhook):
     webhook = json.loads(webhook)
     event = webhook['event']
 
-    # print(webhook)
+    payload = {
+        "clientId": "226a2d88-095c-4469-9943-1a3e6e3ae477",
+        "clientSecret": "58b103c9-2272-4f7d-a1ef-80dd015704dc"}
 
-    ambiente = "Teste"
-
-    if ambiente == "Teste":
-        payload = {
-            "clientId": "226a2d88-095c-4469-9943-1a3e6e3ae477",
-            "clientSecret": "58b103c9-2272-4f7d-a1ef-80dd015704dc"}
-    else:
-        payload = {
-            "clientId": "8e0a0ef7-71f4-4049-ac54-bab15e6c7bb9",
-            "clientSecret": "6ec284c2-cc80-4718-a2d2-5efc1aeb6d52"}
 
     if event == 'transactions/created':
 
