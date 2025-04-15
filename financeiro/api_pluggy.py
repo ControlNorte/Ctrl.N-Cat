dd51724ae62e03ee553387f3c9004184d6e442a8
ControlNorte <ctrl.norte@gmail.com>
2025-04-15 16:27:44 -0300

    0002 - Conexão Pluggy

--- financeiro/api_pluggy.py
+++ financeiro/api_pluggy.py
@@ -169,6 +169,7 @@ def handle_item_data(request):
                 descricao = result['description']
                 valor = result['amount']
                 data = result['date']
+                print(data)
                 data = datetime.strptime(data, '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%Y-%m-%d')
 
                 registro = {
