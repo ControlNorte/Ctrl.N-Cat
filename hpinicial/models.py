5cd82553d7480619fec503ae923d1feb8ea7ebc6
Diogo Silva <ctrl.norte@gmail.com>
2025-06-26 15:26:05 -0300

    0001 - concetando novo db

--- hpinicial/models.py
+++ hpinicial/models.py
@@ -6,8 +6,7 @@ from django.db import models
 class Tenant(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     subdomain = models.CharField(max_length=255, unique=True)
-    cnpj = models.CharField(max_length=255, unique=True)
-    CNPJ = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    cnpj = models.CharField(max_length=255, unique=True, null=True, blank=True)
     ativo = models.BooleanField(null=True, blank=True)
 
     def __str__(self):
